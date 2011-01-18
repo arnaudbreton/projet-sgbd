4bcfba76b439a2191b202ac5d42d9679806524cd
arnaud.breton91 <arnaud.breton91@20b537fc-3dd7-cee2-2afb-2c9a7248ae67>
2011-01-18 17:43:41 +0000

    Algorithme fonctionnel !

--- Projet OLAP/src/regle_association/RechercheRegleAssociation.java
+++ Projet OLAP/src/regle_association/RechercheRegleAssociation.java
@@ -11,37 +11,69 @@ import jdbc.MysqlJDBC;
 
 public class RechercheRegleAssociation {
 
-	public List<RechercheRegleAssociation> getReglesAssociations(String nomTable,
-			double minSup, double minConf) throws Exception {
+	public List<String> getReglesAssociations(
+			String nomTable, double minSup, double minConf) throws Exception {
 		// Connexion à la BDD
 		MysqlJDBC.getInstance().connect();
 
-		List<String> itemsFrequents = getStringsFrequents(nomTable, minSup);
-		
-		List<String> partieGauche = null; 
-		
-		for(String itemFrequent : itemsFrequents) {
+		List<String> itemsFrequents = getAttributsFrequents(nomTable, minSup);
+
+		List<String> reglesInteressantes = new ArrayList<String>();
+		for (String itemFrequent : itemsFrequents) {
+			System.out.println("Début de l'étude de l'ensemble fréquent : " + itemFrequent);
 			List<String> candidats = Arrays.asList(itemFrequent.split(" "));
-		
+
 			int cardinalite = candidats.size();
-			for(int cptCardinalite = 0; cptCardinalite < cardinalite; cptCardinalite++) {
-				partieGauche = genererCandidats(cptCardinalite, candidats);
+			for (int cptCardinalite = 0; cptCardinalite < cardinalite; cptCardinalite++) {
+				List<String> partiesGauches = null;
+				String partieDroite;
+
+				if (cptCardinalite == 0) {
+					partiesGauches = new ArrayList<String>();
+				} else {
+					partiesGauches = genererCandidats(cptCardinalite, candidats);
+				}
+
+				if (partiesGauches.size() == 0) {
+					//getConfiance(nomTable, "", itemFrequent);
+				}
+
+				else {
+					for (String partieGauche : partiesGauches) {
+						partieDroite = itemFrequent;
+						for(String itemPartieGauche : partieGauche.split(" ")) {
+							partieDroite = partieDroite.replace(itemPartieGauche, "");
+						}
+						
+						partieDroite = partieDroite.replaceAll(" +"," ");
+						partieDroite = partieDroite.replaceAll("^ +","");
+						partieDroite = partieDroite.replaceAll(" +$","");
+						
+						String regleEnCours = partieGauche+"=>"+partieDroite;
+						
+						System.out.println("Etude de la règle : " + regleEnCours);
+						double conf = getConfiance(nomTable, partieGauche, partieDroite);
+						
+						System.out.println("Calcul de la confiance de la règle " + regleEnCours + " : " + conf);
+						if(conf > minConf) {
+							reglesInteressantes.add(partieGauche+"=>"+partieDroite);
+							System.out.println(partieGauche+"=>"+partieDroite + " : est intéressante");
+						}
+					}
+				}
 			}
-			
-			candidats.removeAll(partieGauche);
-			
-			getConfiance(nomTable, partieGauche, candidats);
+			System.out.println("Fin de l'étude de l'ensemble fréquent : " + itemFrequent);
 		}
 
 		// Déconnexion de la BDD
 		MysqlJDBC.getInstance().deconnect();
 
-		return null;
+		return reglesInteressantes;
 	}
 
-	private List<String> getStringsFrequents(String nomTable, double minSup)
+	private List<String> getAttributsFrequents(String nomTable, double minSup)
 			throws Exception {
-		
+
 		if (nomTable == null || nomTable == "") {
 			throw new Exception(
 					"Un nom de table est nécessaire pour déterminer les fréquences");
@@ -52,12 +84,13 @@ public class RechercheRegleAssociation {
 		}
 
 		// On récupère l'ensemble des Strings de la table
-		List<String> attsCandidats =  MysqlJDBC.getInstance().getColumnsName(nomTable);
+		List<String> attsCandidats = MysqlJDBC.getInstance().getColumnsName(
+				nomTable);
 		attsCandidats.remove(0);
 
 		List<String> attsFrequents = new ArrayList<String>();
 		ArrayList<String> attsFrequentsN = new ArrayList<String>();
-		
+
 		// On analyse les Strings
 		int cardinalite = 1;
 
@@ -83,12 +116,12 @@ public class RechercheRegleAssociation {
 
 		return attsFrequents;
 	}
-	
+
 	private List<String> genererCandidats(int n, List<String> candidats) {
 		List<String> tempCandidates = new ArrayList<String>(); // temporary
-																	// candidate
-																	// string
-																	// List
+																// candidate
+																// string
+																// List
 		String str1, str2; // strings that will be used for comparisons
 		StringTokenizer st1, st2; // string tokenizers for the two itemsets
 									// being compared
@@ -162,11 +195,9 @@ public class RechercheRegleAssociation {
 
 		return resultRqCountWhere1.getDouble(1);
 	}
-		
 
-	private double getConfiance(String nomTable, List<String> partieGauche, List<String> partieDroite)
-			throws SQLException {
-
-		return 0.0;
+	private double getConfiance(String nomTable, String partieGauche,
+			String partieDroite) throws SQLException, InstantiationException, IllegalAccessException {
+		return getSupport(nomTable, partieGauche.concat(" " + partieDroite)) / getSupport(nomTable, partieGauche);
 	}
 }
