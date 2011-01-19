25f1b40ec753804c5b60fb77be94d6dd47f920c6
flo.gouin <flo.gouin@20b537fc-3dd7-cee2-2afb-2c9a7248ae67>
2011-01-19 18:28:04 +0000

    Ajout de la partie resultat

--- trunk/Projet OLAP/src/swt/Screen.java
+++ trunk/Projet OLAP/src/swt/Screen.java
@@ -15,7 +15,6 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -42,6 +41,8 @@ public class Screen implements Observer{
 	
 	private MysqlJDBC _dataBaseConnection; 
 	private RechercheRegleAssociation _ra;
+	private Table _itemSetsTable;
+	private Table _reglesTable;
 	
 	public Screen(){
 			_dataBaseConnection = MysqlJDBC.getInstance();
@@ -52,12 +53,19 @@ public class Screen implements Observer{
 	public void createContent(){
 		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setLayout(new GridLayout(1, false));
+		shell.setLayout(new GridLayout(2, false));
+		
+		Composite leftPannel = new Composite(shell, SWT.NONE);
+		leftPannel.setLayoutData(new GridData(GridData.FILL_BOTH));
+		leftPannel.setLayout(new GridLayout());
+		Composite rightPannel = new Composite(shell, SWT.NONE);
+		rightPannel.setLayoutData(new GridData(GridData.FILL_BOTH));
+		rightPannel.setLayout(new GridLayout());
 		
 		Label lbl;
 		
 		//Groupe Configuration
-		Group confGroup = new Group(shell,SWT.NONE);
+		Group confGroup = new Group(leftPannel,SWT.NONE);
 		confGroup.setText("Configuration");
 		confGroup.setLayout(new GridLayout(4, false));
 		confGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
@@ -101,7 +109,7 @@ public class Screen implements Observer{
 		});
 		
 		//Groupe de sélection de la table
-		Group tableGroup = new Group(shell,SWT.NONE);
+		Group tableGroup = new Group(leftPannel,SWT.NONE);
 		tableGroup.setText("Table selection");
 		tableGroup.setLayout(new GridLayout(1, false));
 		tableGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
@@ -169,7 +177,7 @@ public class Screen implements Observer{
 		_dataBaseTable.setLayoutData(new GridData(GridData.FILL_BOTH));
 		
 		//Bouton compute pour lancer le calcul
-		Composite btnBarre = new Composite(shell, SWT.RIGHT_TO_LEFT);
+		Composite btnBarre = new Composite(leftPannel, SWT.RIGHT_TO_LEFT);
 		btnBarre.setLayout(new GridLayout());
 		btnBarre.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		Button btnCompute = new Button(btnBarre,SWT.PUSH);
@@ -187,7 +195,12 @@ public class Screen implements Observer{
 					
 					if (minConf>=0 && minConf<=1 && minSup >= 0){
 						try {
-							_ra.getReglesAssociations(_tableName, Double.parseDouble(_minSup), Double.parseDouble(_minConf));
+							_ra.getAttributsFrequents(_tableName, Double.parseDouble(_minSup));
+							_itemSetsTable.clearAll();
+							
+							_ra.getReglesAssociations(_tableName, Double.parseDouble(_minConf));
+							_reglesTable.clearAll();
+							
 						} catch (NumberFormatException e) {
 							e.printStackTrace();
 						} catch (Exception e) {
@@ -198,19 +211,42 @@ public class Screen implements Observer{
 			}
 		});
 		
-		Group traceGrp = new Group (shell, SWT.NONE);
-		traceGrp.setLayout(new FillLayout());
+		Group traceGrp = new Group (leftPannel, SWT.NONE);
+		traceGrp.setLayout(new GridLayout());
 		traceGrp.setLayoutData(new GridData(GridData.FILL_BOTH));
 		traceGrp.setText("Trace Log");
 		
 		_traceLog = new Text(traceGrp, SWT.MULTI | SWT.V_SCROLL);
+		_traceLog.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		
+		//Panneau de droite
+		Group resultGroup = new Group(rightPannel,SWT.NONE);
+		resultGroup.setLayout(new GridLayout(2,false));
+		resultGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
+		resultGroup.setText("Result");
+
+		//Création du tableau de résultat des itemSets
+		_itemSetsTable = new Table(resultGroup,SWT.BORDER | SWT.V_SCROLL);
+		_itemSetsTable.setLinesVisible(true);
+		_itemSetsTable.setHeaderVisible(true);
+		_itemSetsTable.setLayoutData(new GridData(GridData.FILL_BOTH));
+		new TableColumn(_itemSetsTable,SWT.NONE).setText("ItemsSets");
+		new TableColumn(_itemSetsTable,SWT.NONE).setText("Supports");
+		
+		//Création du tableau de résultat des règles
+		_reglesTable = new Table(resultGroup,SWT.BORDER | SWT.V_SCROLL);
+		_reglesTable.setLinesVisible(true);
+		_reglesTable.setHeaderVisible(true);
+		_reglesTable.setLayoutData(new GridData(GridData.FILL_BOTH));
+		new TableColumn(_reglesTable,SWT.NONE).setText("Association rules");
+		new TableColumn(_reglesTable,SWT.NONE).setText("Confidences");
 		
 		shell.pack();
 		shell.open();
-		while (!shell.isDisposed())
-		{
-		if (!display.readAndDispatch())
-		display.sleep();
+		while (!shell.isDisposed()){
+			if (!display.readAndDispatch())
+			display.sleep();
 		}
 		display.dispose();
 	}
